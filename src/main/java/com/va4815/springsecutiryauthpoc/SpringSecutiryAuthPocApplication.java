6ec6a87a621c6e0a5d70feddd739f371da0b6dd6
Vincent <va543567@gmail.com>
2024-12-01 13:37:42 +0000

    feat: initial project with spring security

---
+++ src/main/java/com/va4815/springsecutiryauthpoc/SpringSecutiryAuthPocApplication.java
@@ -0,0 +1,13 @@
+package com.va4815.springsecutiryauthpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecutiryAuthPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecutiryAuthPocApplication.class, args);
+    }
+
+}
